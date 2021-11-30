fdd64265480c8c617b3b51df5b5a077525b3545c
Adam Drago <atdrago@gmail.com>
2021-11-30 15:27:17 -0500

    chore(image proxy): Remove image resizing

--- pages/api/images/proxy.ts
+++ pages/api/images/proxy.ts
@@ -1,5 +1,4 @@
 import { NextApiHandler } from 'next';
-import sharp from 'sharp';
 
 const handler: NextApiHandler = async (req, res) => {
   let image;
@@ -20,16 +19,10 @@ const handler: NextApiHandler = async (req, res) => {
     return;
   }
 
-  const buffer = Buffer.from(await image.arrayBuffer());
-  // Resize image to 512x512 to ensure we stay under the Next 4MB limit
-  const resultBuffer = await sharp(buffer)
-    .resize(512, 512, { fit: sharp.fit.contain })
-    .toBuffer();
-
   res
     .setHeader('Cache-Control', 'public, max-age=31536000, immutable')
     .status(200)
-    .send(resultBuffer);
+    .send(image.body);
 };
 
 export default handler;
