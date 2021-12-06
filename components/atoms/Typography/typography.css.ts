9abd37fd602a395920a120537200ea3e426edfc3
Adam Drago <atdrago@gmail.com>
2021-12-06 10:31:21 -0500

    chore: Adjust Typography line-heights to be closer to WCAG guidelines

--- components/atoms/Typography/typography.css.ts
+++ components/atoms/Typography/typography.css.ts
@@ -36,12 +36,15 @@ function removeBeforeAndAfter<TInput = unknown>(css: TInput): TInput {
   });
 }
 
+const EIGHTEEN_AND_UNDER_LEADING = 1.5;
+const OVER_EIGHTEEN_LEADING = 1.2;
+
 const sizeVariantStyles: Record<string | number, StyleRule> = {
   headingLarge: {
     ...capsize({
       fontMetrics: getFontMetricsFromUserAgent(),
       fontSize: 28,
-      lineGap: 12,
+      leading: 28 * OVER_EIGHTEEN_LEADING,
     }),
     fontSize: vars.typography.fontSize.s28,
   },
@@ -49,7 +52,7 @@ const sizeVariantStyles: Record<string | number, StyleRule> = {
     ...capsize({
       fontMetrics: getFontMetricsFromUserAgent(),
       fontSize: 24,
-      lineGap: 12,
+      leading: 24 * OVER_EIGHTEEN_LEADING,
     }),
     fontSize: vars.typography.fontSize.s24,
   },
@@ -57,7 +60,7 @@ const sizeVariantStyles: Record<string | number, StyleRule> = {
     ...capsize({
       fontMetrics: getFontMetricsFromUserAgent(),
       fontSize: 20,
-      lineGap: 12,
+      leading: 20 * OVER_EIGHTEEN_LEADING,
     }),
     fontSize: vars.typography.fontSize.s20,
   },
@@ -65,7 +68,7 @@ const sizeVariantStyles: Record<string | number, StyleRule> = {
     ...capsize({
       fontMetrics: getFontMetricsFromUserAgent(),
       fontSize: 18,
-      lineGap: 12,
+      leading: 18 * EIGHTEEN_AND_UNDER_LEADING,
     }),
     fontSize: vars.typography.fontSize.s18,
   },
@@ -73,7 +76,7 @@ const sizeVariantStyles: Record<string | number, StyleRule> = {
     ...capsize({
       fontMetrics: getFontMetricsFromUserAgent(),
       fontSize: 12,
-      lineGap: 12,
+      leading: 12 * EIGHTEEN_AND_UNDER_LEADING,
     }),
     fontSize: vars.typography.fontSize.s12,
   },
@@ -87,7 +90,7 @@ const sizeVariantStyles: Record<string | number, StyleRule> = {
     ...capsize({
       fontMetrics: getFontMetricsFromUserAgent(),
       fontSize: 16,
-      lineGap: 16,
+      leading: 16 * EIGHTEEN_AND_UNDER_LEADING,
     }),
     fontSize: vars.typography.fontSize.s16,
     selectors: {
