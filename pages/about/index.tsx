fc85102789bbfb161e4204fb6307fe8c2272a3cf
Adam Drago <atdrago@gmail.com>
2021-12-11 19:33:03 -0500

    fix(about): Remove duplicate header on about page

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -4,7 +4,6 @@ import type { FunctionComponent } from 'react';
 import { Head } from 'components/atoms/Head';
 import { Typography } from 'components/atoms/Typography';
 import { Stack } from 'components/layouts/Stack';
-import { Header } from 'components/molecules/Header';
 
 export const getStaticProps: GetStaticProps = () => {
   return {
@@ -19,7 +18,6 @@ const AboutPage: FunctionComponent = () => {
         titles={['About']}
         description={`Hello, and welcome to ${process.env.NEXT_PUBLIC_APP_NAME}!`}
       />
-      <Header />
       <Stack as="main">
         <Typography as="p" size="paragraph">
           Hello, and welcome to {process.env.NEXT_PUBLIC_APP_NAME}!
