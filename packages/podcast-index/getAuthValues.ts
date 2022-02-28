b9942fc58008e2f92f9c1060c941b6d1a27c0c42
Adam Drago <atdrago@gmail.com>
2022-02-28 17:01:51 -0500

    chore: Adjust comment on `getAuthValues`

--- packages/podcast-index/getAuthValues.ts
+++ packages/podcast-index/getAuthValues.ts
@@ -3,7 +3,8 @@ import crypto from 'crypto';
 /**
  * Returns auth values based on the provided Podcast Index API Key and Secret.
  * Because this function requires the API Secret, it should not be called on the
- * client. Referencing it on the client will add 165kB to the build
+ * client. Referencing it on the client will also add 165kB to the build because
+ * of the need to polyfill the `crypto` library.
  * @param apiKey
  * @param apiSecret
  * @returns An array where the first item is auth time in seconds, and the
