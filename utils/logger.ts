6f752393e471bf53d88e3aa74f225a3482ec506b
Dan Dombrowski <djdmbrwsk@gmail.com>
2022-03-04 17:37:41 -0500

    chore: Disable Logflare when variables missing (#185)

--- utils/logger.ts
+++ utils/logger.ts
@@ -1,10 +1,21 @@
 import { logflarePinoVercel } from '@atdrago/pino-logflare';
 import pino from 'pino';
 
-const { send, stream } = logflarePinoVercel({
-  apiKey: process.env.NEXT_PUBLIC_LOGFLARE_API_KEY,
-  sourceToken: process.env.NEXT_PUBLIC_LOGFLARE_SOURCE_TOKEN,
-});
+const sendToLogflare =
+  !!process.env.NEXT_PUBLIC_LOGFLARE_API_KEY &&
+  !!process.env.NEXT_PUBLIC_LOGFLARE_SOURCE_TOKEN;
+
+const { send, stream } = sendToLogflare
+  ? logflarePinoVercel({
+      apiKey: process.env.NEXT_PUBLIC_LOGFLARE_API_KEY,
+      sourceToken: process.env.NEXT_PUBLIC_LOGFLARE_SOURCE_TOKEN,
+    })
+  : {
+      send: () => {
+        // noop
+      },
+      stream: process.stdout,
+    };
 
 export const logger = pino(
   {
@@ -13,10 +24,12 @@ export const logger = pino(
       revision: process.env.VERCEL_GITHUB_COMMIT_SHA,
     },
     browser: {
-      transmit: {
-        level: 'info',
-        send,
-      },
+      transmit: sendToLogflare
+        ? {
+            level: 'info',
+            send,
+          }
+        : undefined,
       write: () => {
         // Prevent logs from being output to the browser console
       },
