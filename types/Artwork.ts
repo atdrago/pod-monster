0454cb3596ec29eaafe59c4c80684d75d583845b
Adam Drago <atdrago@gmail.com>
2023-10-01 15:20:14 -0400

    fix lint

--- types/Artwork.ts
+++ types/Artwork.ts
@@ -1,4 +1,4 @@
-import Image, { ImageProps } from 'next/image';
+import { ImageProps } from 'next/image';
 import type { PolymorphicComponent } from 'react-polymorphic-box';
 
 import { shadowVariant } from 'components/atoms/Artwork/artwork.css';
