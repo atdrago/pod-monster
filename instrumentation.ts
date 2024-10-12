aef045a06418bffdb396aed5e4ebea2d7c0ed4b5
Adam Drago <atdrago@gmail.com>
2024-10-11 17:41:39 -0700

    feat: Update Next.js to 14

---
+++ instrumentation.ts
@@ -0,0 +1,13 @@
+import * as Sentry from '@sentry/nextjs';
+
+export function register() {
+  Sentry.init({
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+
+    dsn: 'https://231128c2ea5ad7c5e3b0ad63d10e61e2@o949194.ingest.sentry.io/4505975815208960',
+
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
+  });
+}
--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -12,7 +12,7 @@ Sentry.init({
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
-    new Sentry.Replay({
+    Sentry.replayIntegration({
       blockAllMedia: true,
       // Additional Replay configuration goes in here, for example:
       maskAllText: true,
