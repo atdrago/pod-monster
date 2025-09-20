05d182b6d3d7fd6cd372e29709430c59cf0e6233
Adam Drago <atdrago@gmail.com>
2025-09-20 01:05:05 -0500

    Add VERCEL_ENV types

--- declarations/ProcessEnv.d.ts
+++ declarations/ProcessEnv.d.ts
@@ -3,5 +3,9 @@ declare namespace NodeJS {
     NEXT_PUBLIC_BASE_URL: string;
     NEXT_PUBLIC_PODCAST_INDEX_API_KEY: string;
     NEXT_PUBLIC_PODCAST_INDEX_API_SECRET: string;
+    /**
+     * https://vercel.com/docs/environment-variables/system-environment-variables#VERCEL_ENV
+     */
+    VERCEL_ENV: 'production' | 'development' | 'preview';
   }
 }
