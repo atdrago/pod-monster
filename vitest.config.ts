b09fc9325a41492d06c2e980cdc759c775b84c02
Adam Drago <atdrago@gmail.com>
2025-11-17 23:02:03 -0500

    chore: Switch from jest to vitest (#470)
    * chore: Switch from jest to vitest
    
    * fixes

---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+// eslint-disable-next-line import/no-unused-modules
+export default defineConfig({
+  // @ts-expect-error workaround for moduleResolution: 'bundler' issue
+  plugins: [tsconfigPaths(), react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './vitest.setup.ts',
+  },
+});
