61fb9629406d4148d66546dc34e074c17f73bd9c
Adam Drago <atdrago@gmail.com>
2025-11-19 00:09:40 -0500

    remove svg mocks in setup

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,15 +1,3 @@
 import 'styles/app.css';
 import 'styles/globals.css';
 import '@testing-library/jest-dom';
-import { vi } from 'vitest';
-
-// Mock SVG imports
-vi.mock('*.svg', () => {
-  const MockSvg = () => 'svg';
-
-  return {
-    ReactComponent: MockSvg,
-    __esModule: true,
-    default: MockSvg,
-  };
-});
