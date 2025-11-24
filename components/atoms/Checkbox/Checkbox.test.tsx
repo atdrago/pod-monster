0290bc4d8d9edfc72da0fce6d33edab23f360d0f
Adam Drago <atdrago@gmail.com>
2025-11-24 08:45:05 -0500

    chore(vitest): Use vite-plugin-svgr to render SVGs

--- components/atoms/Checkbox/Checkbox.test.tsx
+++ components/atoms/Checkbox/Checkbox.test.tsx
@@ -1,15 +1,6 @@
 import { render } from '@testing-library/react';
-import { vi } from 'vitest';
 
 import { Checkbox } from './Checkbox';
-
-vi.mock('icons/checkmark.svg', () => ({
-  default: () => (
-    <svg viewBox="0 0 32 32">
-      <path d="M27 4l-15 15-7-7-5 5 12 12 20-20z" />
-    </svg>
-  ),
-}));
 
 describe('Checkbox', () => {
   it('should render unchecked checkbox', async () => {
